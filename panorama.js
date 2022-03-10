d0bbe11ef9c9f99a63ee28d6a656b53bc8afb62c
j0rdan2k <58141965+j0rdan2k@users.noreply.github.com>
2022-03-10 18:48:23 +0100

    Update panorama.js

--- panorama.js
+++ panorama.js
@@ -61,11 +61,11 @@ var Lobbies = {
                             <Image id="JsFriendAdvertisePrime"
                                     class="friendlobby__row__icon-prime"
                                     src="file://{images}/icons/ui/prime.svg"
-                                    texturewidth="10"
-                                    textureheight="10"/>
+                                    texturewidth="20"
+                                    textureheight="20"/>
                             <Image id="JsFriendAdvertiseSkillGroup"
                                     class="friendlobby__row__icon-skillgroup" 
-                                    textureheight="10"
+                                    textureheight="20"
                                     texturewidth="-1"
                                     defaultsrc="" />
                             <Label id="JsRegionLabel" class="" text=""/>
